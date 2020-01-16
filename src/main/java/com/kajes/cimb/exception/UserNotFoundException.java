999ea99a0db3334694e3516c09c0a35267bbc6cd
kajeshkamma <kajeshkamma@users.noreply.github.com>
2020-01-16 13:02:00 +0800

    changes

--- src/main/java/com/kajes/cimb/exception/UserNotFoundException.java
+++ src/main/java/com/kajes/cimb/exception/UserNotFoundException.java
@@ -6,9 +6,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends Exception{
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 8096752469131146597L;
 	
 	public UserNotFoundException(String message){
